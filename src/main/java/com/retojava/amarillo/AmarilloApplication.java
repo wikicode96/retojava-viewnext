56a4f5455f404bbfdb0cdc2c78c493d194c9b4a5
Carlos Soler García <wikicode96@gmail.com>
2025-01-29 10:52:27 +0100

    first commit

---
+++ src/main/java/com/retojava/amarillo/AmarilloApplication.java
@@ -0,0 +1,13 @@
+package com.retojava.amarillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retojava/amarillo/AmarilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retojava.amarillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmarilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
