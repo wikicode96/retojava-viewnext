d0116945fae6af0f15514f4d2f8636fc50428843
Kai-TRD <wkailuo03@gmail.com>
2025-01-29 12:54:09 +0100

    Respositori

---
+++ src/main/java/com/retojava/amarillo/repository/CarteristaRepository.java
@@ -0,0 +1,9 @@
+package com.retojava.amarillo.repository;
+
+import com.retojava.amarillo.models.Carterista;
+
+public class CarteristaRepository {
+	public interface UsuarioRepository extends JpaRepository<Carterista, Long> {
+	}
+
+}
---
+++ src/main/java/com/retojava/amarillo/repository/LugarCarteristaRepository.java
@@ -0,0 +1,5 @@
+package com.retojava.amarillo.repository;
+
+public class LugarCarteristaRepository {
+
+}
