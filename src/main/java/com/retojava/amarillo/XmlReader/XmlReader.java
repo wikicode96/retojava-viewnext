6da9808b57103c7819533f1d7e7e6607bb743ef0
Carlos Soler García <wikicode96@gmail.com>
2025-01-29 12:36:58 +0100

    fixing component XmlReader

--- src/main/java/com/retojava/amarillo/XmlReader/XmlReader.java
+++ src/main/java/com/retojava/amarillo/XmlReader/XmlReader.java
@@ -1,10 +1,12 @@
 package com.retojava.amarillo.XmlReader;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 
+@Component
 public class XmlReader {
     public static <T> T fileReader(String filePath, Class<T> xmlObject) throws IOException {
         File fileXml = new File(filePath);
